8c0d51d984ce62f31aac884116838d41b501c653
tim-sebold <tims@rizeprevention.org>
2026-08-16 07:01:34 +0200

    fix: update Firebase config and certificate verification

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -61,8 +61,8 @@ export const downloadCertificate = async (): Promise<Blob> => {
     });
 };
 
-export const makeCertificateId = async () => {
-    const year = new Date().getFullYear;
+export const makeCertificateId = () => {
+    const year = new Date().getFullYear();
     const rand = Math.floor(100000 + Math.random() * 900000);
     return `IJ-${year}-${rand}`;
 }
